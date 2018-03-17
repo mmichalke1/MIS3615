307b39967eb3a7302380cca1863bdb69796a78ad
mmichalke1 <36529110+mmichalke1@users.noreply.github.com>
2018-03-17 16:15:18 -0400

    did test stuff to hello.py

---
+++ hello.py
@@ -0,0 +1,17 @@
+print('\nhello, it is me')
+print("\tI was wondering if after all these years you'd like to meet")
+
+print(1+2+3+4)
+
+print(10/3)
+
+print(2**3)
+
+print(2**10)
+
+print(2+02)
+
+from math import sqrt
+n= 100
+def F(n):
+    return ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
